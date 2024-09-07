ade5b39ee97271c473682bc8bf4da3a8c3b4c4ba
Калинин Игорь <lapis-lines@stud.nstu.ru>
2024-09-07 15:12:16 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Иванов Иван Иванович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1 
+        </header>
+    
+    <hl>web-сервер на flask</hl>
+
+    <footer>
+        &copу; Калинин Игорь, ФБИ-23, 3 курс, 2024
+    </footer>
+    </body>
+</html>
+"""
